11bb40b907ce47ec85a307d25ba3c2ff668846d5
Xlient <lakaleigh.harris@gmail.com>
2021-09-22 14:00:38 -0400

    Basic api added two controllers added dependencies for MYSQL

---
+++ MapAPI/Controllers/OrganizationController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MapAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationController : ControllerBase
+    {
+        // GET: api/<OrganizationController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<OrganizationController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrganizationController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OrganizationController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrganizationController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
